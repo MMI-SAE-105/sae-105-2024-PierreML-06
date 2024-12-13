f1837233075562b9e64149251675bb1d935f50f4
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2024-12-13 13:46:48 +0100

    formulaire

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,6 @@ if (toggle && nav) {
     toggle.addEventListener("click", () => {
         const isOpen = toggle.ariaExpanded === "true";
         const isClosed = !isOpen;
-        // Mise à jour des attributs ARIA pour accessibilité
         toggle.ariaExpanded = isClosed;
         nav.ariaHidden = isOpen;
         page.classList.toggle("noscroll", isClosed);
@@ -20,22 +19,24 @@ if (toggle && nav) {
 
 
 
-// *** CAROUSEL ***
-// TODO
 const carousel = document.querySelector('.carousel__container');
 const prevButton = document.querySelector('.carousel__button--prev');
 const nextButton = document.querySelector('.carousel__button--next');
 
-// Largeur de défilement d’un item
 if (carousel) {
-    // Scroll au clic sur le bouton précédent
     prevButton.addEventListener("click", () => {
         carousel.scrollLeft -= carousel.offsetWidth;
     });
 
-    // Scroll au clic sur le bouton suivant
     nextButton.addEventListener("click", () => {
         carousel.scrollLeft += carousel.offsetWidth;
     });
 }
 
+
+// formulaire de contacte ---------------------------------------------
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Empêche la soumission réelle du formulaire
+    alert("Votre message a été envoyé !");
+});
