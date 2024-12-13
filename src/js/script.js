4585d040f697efef8b8fd3df6db33bade9a950ca
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2024-12-13 11:19:09 +0100

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// On cible les éléments à modifier
+// menu --------------------------------------------------------------------
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const page = document.body;
@@ -14,3 +14,28 @@ if (toggle && nav) {
         page.classList.toggle("noscroll", isClosed);
     });
 }
+
+
+// carousel-------------------------------------------------------------
+
+
+
+// *** CAROUSEL ***
+// TODO
+const carousel = document.querySelector('.carousel__container');
+const prevButton = document.querySelector('.carousel__button--prev');
+const nextButton = document.querySelector('.carousel__button--next');
+
+// Largeur de défilement d’un item
+if (carousel) {
+    // Scroll au clic sur le bouton précédent
+    prevButton.addEventListener("click", () => {
+        carousel.scrollLeft -= carousel.offsetWidth;
+    });
+
+    // Scroll au clic sur le bouton suivant
+    nextButton.addEventListener("click", () => {
+        carousel.scrollLeft += carousel.offsetWidth;
+    });
+}
+
